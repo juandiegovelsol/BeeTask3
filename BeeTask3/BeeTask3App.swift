0bfcbc1a50ce165928bcf211fe1be63643ac0fd8
Juan Diego Velasco <juandiegovelsol@gmail.com>
2024-07-06 13:02:54 -0500

    Initial Commit

---
+++ BeeTask3/BeeTask3App.swift
@@ -0,0 +1,17 @@
+//
+//  BeeTask3App.swift
+//  BeeTask3
+//
+//  Created by Angela maria Erazo lozano on 6/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BeeTask3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
